218b24ca8ed2622729027142f9ae6af9c359e3c4
Addison Martin <addisonmartin@hey.com>
2021-01-26 10:32:55 -0500

    Installed and configured TailwindCSS.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  plugins: [
+  plugins:
+  [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -7,6 +8,8 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
+    require('tailwindcss'),
+    require('autoprefixer')
   ]
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  darkMode: 'media', // or 'False' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
